3cbc17ea2df0421e35ad37d4b9d5efaaa44415b4
Matt Wecht <mattwecht@gmail.com>
2020-05-21 14:42:11 -0400

    Initial setup

---
+++ TextResponseApp/Contact.swift
@@ -0,0 +1,14 @@
+//
+//  Contact.swift
+//  TextResponseApp
+//
+//  Created by Matthew Wecht on 5/21/20.
+//  Copyright © 2020 Matthew Wecht. All rights reserved.
+//
+
+import UIKit
+
+class Contact {
+    var name = " "
+    var number = " "
+}
---
+++ TextResponseApp/ContactsTableViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ContactsTableViewController.swift
+//  TextResponseApp
+//
+//  Created by Matthew Wecht on 5/21/20.
+//  Copyright © 2020 Matthew Wecht. All rights reserved.
+//
+
+import UIKit
+
+class ContactsTableViewController: UITableViewController {
+
+    var contacts : [Contact] = []//sets up empty array
+
+    func createContacts() -> [Contact] {
+
+      let sample = Contact()
+        sample.name = "sally"
+        sample.number = "215-356-0306"
+        
+      return [sample]//returns array of contacts
+    }//end createContacts function
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        contacts = createContacts()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return contacts.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let contact = contacts[indexPath.row]
+        
+        cell.textLabel?.text = contact.name
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
